19d213bcb7273d152d7a0b38dfa1a132e6c21241
Jordan Scales <scalesjordan@gmail.com>
2013-06-29 15:03:28 -0700

    Only attempt to submit a message if we specify content - better UX

--- client/views/messages/message_form.js
+++ client/views/messages/message_form.js
@@ -6,11 +6,13 @@ Template.messageForm.events({
       content: $(e.target).find('[name=content]').val()
     };
 
-    Meteor.call('message', message, function (error, id) {
-      if (error)
-        alert(error.reason)
-      else
-        $(e.target).find('[name=content]').val('')
-    });
+    if (message.content) {
+      Meteor.call('message', message, function (error, id) {
+        if (error)
+          alert(error.reason)
+        else
+          $(e.target).find('[name=content]').val('')
+      });
+    }
   }
 });
